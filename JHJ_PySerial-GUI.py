05479cd7e7bd11f886910a5ce18c0336ce531da2
janghanju <hanju1215@naver.com>
2026-07-01 21:19:08 +0900

    Upgrade version to v1.0.0.8 and optimize PyInstaller build settings

--- JHJ_PySerial-GUI.py
+++ JHJ_PySerial-GUI.py
@@ -148,7 +148,7 @@ class SerialDataloggerApp(QMainWindow):
     """현대적인 다크 플랫 테마 스타일의 메인 윈도우 클래스"""
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("[HelixKorea JHJ] Serial Datalogger v1.0.0.7")
+        self.setWindowTitle("[HelixKorea JHJ] Serial Datalogger v1.0.0.8")
         self.resize(1100, 900)
 
         # 아이콘 설정
